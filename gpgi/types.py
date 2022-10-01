6160ae301104b9b98dc22234999cdee36772bd5c
Clément Robert <cr52@protonmail.com>
2022-10-01 22:07:21 +0200

    ENH: validate dataset coordinate consistency

--- gpgi/types.py
+++ gpgi/types.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import enum
 from abc import ABC
 from abc import abstractmethod
-from dataclasses import dataclass
 from functools import cached_property
 from functools import reduce
 from typing import Any
@@ -36,13 +35,11 @@ FieldMap = dict[Name, np.ndarray]
 
 class SpatialData(Protocol):
     geometry: Geometry
-    coordinates: FieldMap
     axes: tuple[Name, ...]
 
 
 class ValidatorMixin(SpatialData, ABC):
     def __init__(self) -> None:
-        self.axes = tuple(self.coordinates.keys())
         self._validate()
 
     @abstractmethod
@@ -145,6 +142,7 @@ class Grid(ValidatorMixin):
         self.coordinates = cell_edges
         self.fields = fields
 
+        self.axes = tuple(self.coordinates.keys())
         super().__init__()
 
     def _validate(self) -> None:
@@ -193,6 +191,7 @@ class ParticleSet(ValidatorMixin):
         self.coordinates = coordinates
         self.fields = fields
 
+        self.axes = tuple(self.coordinates.keys())
         super().__init__()
 
     def _validate(self) -> None:
@@ -210,11 +209,31 @@ class ParticleSet(ValidatorMixin):
         return len(self.axes)
 
 
-@dataclass
-class Dataset:
-    geometry: Geometry = Geometry.CARTESIAN
-    grid: Grid | None = None
-    particles: ParticleSet | None = None
+class Dataset(ValidatorMixin):
+    def __init__(
+        self,
+        *,
+        geometry: Geometry = Geometry.CARTESIAN,
+        grid: Grid | None = None,
+        particles: ParticleSet | None = None,
+    ):
+        self.geometry = geometry
+        self.grid = grid
+        self.particles = particles
+
+        super().__init__()
+
+    def _validate(self) -> None:
+        if self.grid is None or self.particles is None:
+            return
+        for ax, edges in self.grid.cell_edges.items():
+            domain_left = edges[0]
+            domain_right = edges[-1]
+            for x in self.particles.coordinates[ax]:
+                if x < domain_left:
+                    raise ValueError(f"Got particle at {ax}={x} < {domain_left=}")
+                if x > domain_right:
+                    raise ValueError(f"Got particle at {ax}={x} > {domain_right=}")
 
     def _setup_host_cell_index(self) -> None:
         if hasattr(self, "_hci") or self.particles is None or self.grid is None:
