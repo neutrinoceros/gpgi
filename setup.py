9c9fcff084ad369cb51a7c9d6be2435eaa9825a1
Clément Robert <cr52@protonmail.com>
2023-03-02 11:27:33 +0100

    BLD: forbid deprecated numpy C API

--- setup.py
+++ setup.py
@@ -14,11 +14,13 @@ setup(
                 "gpgi.clib._indexing",
                 ["gpgi/clib/_indexing.pyx"],
                 include_dirs=[numpy.get_include()],
+                define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
             ),
             Extension(
                 "gpgi.clib._deposition_methods",
                 ["gpgi/clib/_deposition_methods.pyx"],
                 include_dirs=[numpy.get_include()],
+                define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
             ),
         ],
         compiler_directives={"language_level": 3},
