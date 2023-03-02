a8a6b1dc3aab1f289b0e2f6d16a6342905a01b6c
Clément Robert <cr52@protonmail.com>
2023-03-02 14:22:15 +0100

    BLD: simplify build instructions

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
                 define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
             ),
         ],
-        compiler_directives={"language_level": 3},
-        annotate=True,
+        # annotate=True, # uncomment to produce html reports
     ),
 )
